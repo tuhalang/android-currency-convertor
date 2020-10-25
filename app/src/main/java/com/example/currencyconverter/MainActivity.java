a48572b5dce8540f9d5a5bc35c4f9f8e4c9e2551
tuhalang <tuhalang007@outlook.com.vn>
2020-10-25 21:03:54 +0700

    add file

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,71 +3,96 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    EditText editUSD, editEUR, editVND;
+    String[] currency = { "USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "HKD", "NZD"};
+    double[] toVNDs = { 23177, 27493.72, 221.36, 30235.53, 16546.06, 17649.95, 25634.2, 3466.09, 2990.5, 15461.23};
 
-    double usd2eur = 0.84;
-    double usd2vnd = 23178.0;
+    Spinner from, to;
+    EditText amount;
+    TextView result;
+
+    double toVND = toVNDs[0], fromVND= toVNDs[0];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editUSD = findViewById(R.id.edit_usd);
-        editEUR = findViewById(R.id.edit_eur);
-        editVND = findViewById(R.id.edit_vnd);
+        from = (Spinner) findViewById(R.id.from);
+        to = (Spinner) findViewById(R.id.to);
+        amount = findViewById(R.id.amount);
+        result = findViewById(R.id.result);
+
+        ArrayAdapter<String> aac = new ArrayAdapter(this,android.R.layout.simple_spinner_item,currency);
+        aac.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        from.setOnItemSelectedListener(this);
+        from.setAdapter(aac);
+
+        to.setOnItemSelectedListener(this);
+        to.setAdapter(aac);
+
+        amount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                exchange();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) { }
+        });
 
         findViewById(R.id.button_clear).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editUSD.setText("");
-                editEUR.setText("");
-                editVND.setText("");
+                amount.setText("");
+                result.setText("");
             }
         });
+    }
 
-        findViewById(R.id.button_convert).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    if (editUSD.hasFocus()) {
-                        Log.v("TAG", "Convert USD");
-                        double usd = Double.parseDouble(editUSD.getText().toString());
-                        Log.v("TAG", "USD = " + usd);
-                        double eur = usd * usd2eur;
-                        double vnd = usd * usd2vnd;
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Spinner spin = (Spinner)adapterView;
 
-                        editEUR.setText(String.format("%.2f", eur));
-                        editVND.setText(String.format("%.0f", vnd));
-                    } else if (editEUR.hasFocus()) {
-                        Log.v("TAG", "Convert EUR");
-                        double eur = Double.parseDouble(editEUR.getText().toString());
-                        Log.v("TAG", "EUR = " + eur);
-                        double usd = eur / usd2eur;
-                        double vnd = usd * usd2vnd;
+        if(spin.getId() == R.id.from)
+        {
+            toVND = toVNDs[i];
+        }else if(spin.getId() == R.id.to)
+        {
+            fromVND = 1/toVNDs[i];
+        }
 
-                        editUSD.setText(String.format("%.2f", usd));
-                        editVND.setText(String.format("%.0f", vnd));
-                    } else if (editVND.hasFocus()) {
-                        Log.v("TAG", "Convert VND");
-                        double vnd = Double.parseDouble(editVND.getText().toString());
-                        Log.v("TAG", "VND = " + vnd);
-                        double usd = vnd / usd2vnd;
-                        double eur = usd * usd2eur;
+        exchange();
+    }
 
-                        editEUR.setText(String.format("%.2f", eur));
-                        editUSD.setText(String.format("%.2f", usd));
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    void exchange(){
+        String amountString = amount.getText().toString();
+        if(!amountString.equals("")) {
+            double amountFrom = Double.parseDouble(amountString);
+            double amountResult = amountFrom * fromVND * toVND;
+
+            result.setText(String.valueOf(amountResult));
+        }else{
+            result.setText("");
+        }
     }
 }
